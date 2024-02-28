f621a75ab8af85a4b0b43051d77f92629a4fdba6
Vineet Verma <vineetv44@gmail.com>
2024-02-28 16:58:19 +0530

    Send Event Properties

--- app/src/main/java/com/vineet/retirementcalculator/MainActivity.kt
+++ app/src/main/java/com/vineet/retirementcalculator/MainActivity.kt
@@ -13,10 +13,42 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding =ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        AppCenter.start(application, "384d87d3-7f39-4f21-bb0b-8b28f648dea1", Analytics::class.java, Crashes::class.java)
+        AppCenter.start(
+            application,
+            "384d87d3-7f39-4f21-bb0b-8b28f648dea1",
+            Analytics::class.java,
+            Crashes::class.java
+        )
+        binding.apply {
+            calculateButton.setOnClickListener {
+//            Crashes.generateTestCrash()
+                try {
+                    val interestRate = interestEditText.text.toString().toFloat()
+                    val currentAge = ageEditText.text.toString().toInt()
+                    val retirementAge = retirementEditText.text.toString().toInt()
+                    val monthly = monthlySavingsEditText.text.toString().toFloat()
+                    val current = currentEditText.text.toString().toFloat()
 
+                    val properties:HashMap<String, String> = HashMap<String, String>()
+                    properties.put("interest_rate", interestRate.toString())
+                    properties.put("current_age", currentAge.toString())
+                    properties.put("retirement_age", retirementAge.toString())
+                    properties.put("monthly_savings", monthly.toString())
+                    properties.put("current_savings", current.toString())
+
+                    if (interestRate <= 0) {
+                        Analytics.trackEvent("wrong interest rate", properties)
+                    }
+                    if (retirementAge <= currentAge) {
+                        Analytics.trackEvent("wrong age", properties)
+                    }
+                } catch (e: Exception) {
+                    Analytics.trackEvent(e.message)
+                }
+            }
+        }
     }
 }
